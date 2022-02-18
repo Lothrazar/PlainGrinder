eb26750489b09d97307e81de2c51b9e354f8445b
lothrazar <samson.bassett@gmail.com>
2022-02-18 11:08:34 -0800

    1.2.3 release https://www.curseforge.com/minecraft/mc-mods/plain-grinder/files/3551117

--- src/main/java/com/lothrazar/plaingrinder/grind/TileGrinder.java
+++ src/main/java/com/lothrazar/plaingrinder/grind/TileGrinder.java
@@ -105,12 +105,12 @@ public class TileGrinder extends BlockEntity implements MenuProvider, Container 
   }
 
   @Override
-  public CompoundTag save(CompoundTag tag) {
+  public void saveAdditional(CompoundTag tag) {
     tag.put(NBTINV, inventory.serializeNBT());
     tag.putInt("grindstage", stage);
     tag.putInt("timer", timer);
     tag.putInt("emptyHits", emptyHits);
-    return super.save(tag);
+    super.saveAdditional(tag);
   }
 
   @Override
